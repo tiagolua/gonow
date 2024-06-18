f9aff35f738753e96042a7e833e4c29a7bdbd9ef
Tiago Luã <65502817+tiagolua@users.noreply.github.com>
2024-06-17 22:47:11 -0300

    create config/schemas

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/tiagolua/gonow.git/router"
+import (
+	"errors"
+
+	"github.com/tiagolua/gonow.git/config"
+	"github.com/tiagolua/gonow.git/router"
+)
 
 func main() {
+	// initialize config
+    err error := config.Init()	
+	if err != nil{
+		panic(error)
+	}
+
 	//initialize router
 	router.Initializer()
 }
